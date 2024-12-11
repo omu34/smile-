d437204b120007a2540aff7ad793eecc72e8b69f
omu34 <bernardomuse22@gmail.com>
2024-12-11 17:25:04 +0300

    tailwind bundled

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./js/**/*.js", "./css/**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
